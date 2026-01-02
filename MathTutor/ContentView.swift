d58fd812ecc42dc596364c2812e42b9a33ee2943
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-02 19:42:29 +0000

    Random generation of strings of emoji to match numbers in question

--- MathTutor/ContentView.swift
+++ MathTutor/ContentView.swift
@@ -13,17 +13,36 @@ struct ContentView: View {
     @State private var firstNumber: Int = 0
     @State private var secondNumber: Int = 0
     
+    @State private var firstNumberEmojis = ""
+    @State private var secondNumberEmojis = ""
+    
     private let emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜🏽‍♀️", "🧙🏿‍♂️", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋", "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
     
     var body: some View {
         VStack {
+            VStack {
+                Text(firstNumberEmojis)
+                Text("+")
+                Text(secondNumberEmojis)
+            }
+            .font(.system(size: 80))
+            .multilineTextAlignment(.center)
+            
+            Spacer()
+            
             Text("\(firstNumber) + \(secondNumber) =")
                 .font(.largeTitle)
+            let _ = print(firstNumberEmojis)
+            let _ = print(secondNumberEmojis)
         }
         .padding()
         .onAppear {
             firstNumber = Int.random(in: 1...10)
             secondNumber = Int.random(in: 1...10)
+            
+            firstNumberEmojis =  String(repeating: emojis[Int.random(in: 0..<emojis.count)], count: firstNumber)
+            secondNumberEmojis =  String(repeating: emojis[Int.random(in: 0..<emojis.count)], count: secondNumber)
+            
         }
     }
 }
