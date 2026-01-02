babb635509114ec6fc3cdbc21c6bfc7f4add6e91
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-02 20:13:01 +0000

    Basic function to understand correct answer with message implemented

--- MathTutor/ContentView.swift
+++ MathTutor/ContentView.swift
@@ -7,16 +7,21 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     
     @State private var firstNumber: Int = 0
     @State private var secondNumber: Int = 0
-    
     @State private var firstNumberEmojis = ""
     @State private var secondNumberEmojis = ""
-    
     @State private var answer: String = ""
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var textfieldDisabled = false
+    @State private var guessButtonDisabled = false
+    @State private var message = ""
+    
+    @FocusState private var isTextfieldFocused: Bool
     
     private let emojis = ["🍕", "🍎", "🍏", "🐵", "👽", "🧠", "🧜🏽‍♀️", "🧙🏿‍♂️", "🥷", "🐶", "🐹", "🐣", "🦄", "🐝", "🦉", "🦋", "🦖", "🐙", "🦞", "🐟", "🦔", "🐲", "🌻", "🌍", "🌈", "🍔", "🌮", "🍦", "🍩", "🍪"]
     
@@ -41,18 +46,44 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .textFieldStyle(.roundedBorder)
                 .keyboardType(.numberPad)
+                .focused($isTextfieldFocused)
                 .frame(width: 60)
                 
                 .overlay {
                     RoundedRectangle(cornerRadius: 5)
                         .stroke(.gray, lineWidth: 2)
                 }
+                .disabled(textfieldDisabled)
             
             Button("Guess") {
+                isTextfieldFocused = false
                 
+                guard let answerValue = Int(answer) else {
+                    return
+                }
+                
+                if answerValue == (firstNumber + secondNumber) {
+                    playSound(soundName: "correct")
+                    message = "Correct!"
+                } else {
+                    playSound(soundName: "wrong")
+                    message = "Sorry, the correct answer is \(firstNumber + secondNumber)!"
+                }
+                textfieldDisabled = true
+                guessButtonDisabled = true
             }
             .buttonStyle(.borderedProminent)
-            .disabled(answer.isEmpty)
+            .disabled(answer.isEmpty || guessButtonDisabled)
+            
+            Spacer()
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(message == "Correct!" ? .green : .red)
+                .frame(height: 100)
+                .minimumScaleFactor(0.5)
         }
         .padding()
         .onAppear {
@@ -62,6 +93,24 @@ struct ContentView: View {
             firstNumberEmojis =  String(repeating: emojis[Int.random(in: 0..<emojis.count)], count: firstNumber)
             secondNumberEmojis =  String(repeating: emojis[Int.random(in: 0..<emojis.count)], count: secondNumber)
             
+        }
+    }
+    
+    func playSound(soundName: String) {
+        
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
         }
     }
 }
